cb76763ef66bcbb0eac4320dc6ad1a8aa5917b8a
moises1409 <127446413+moises1409@users.noreply.github.com>
2024-09-07 13:32:36 +0200

    subida de primera funcion openai

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
+from fonctions import *
+from dotenv import load_dotenv
+import os
+from openai import OpenAI
+from pydantic import BaseModel
 
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route("/", methods=['GET'])
 def get_test():
     return "holass mundo"
+
+@app.route("/get_story", methods=['GET'])
+def get_story():
+    topic= "una nina llamada Isabel tiene super poderes"
+    language = "Spanish"
+    response = generate_story(topic, language)
+    return jsonify(response)
+    
+    
+    
 
 if __name__ == "__main__":
     app.run
