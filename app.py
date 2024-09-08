44ca08a6d4c208443abdc8d3616dd25d94fb56da
moises1409 <127446413+moises1409@users.noreply.github.com>
2024-09-08 19:38:31 +0200

    no funciona3

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ app = Flask(__name__)
 CORS(app)
 
 # API Clients and keys
-#openai_api_key = os.getenv("OPENAI_API_KEY")
+openai_api_key = os.getenv("OPENAI_API_KEY")
 
 # Initialize API client
-#client = OpenAI(api_key=openai_api_key)
+client = OpenAI(api_key=openai_api_key)
 
 # Constants
 PROMPT_SYSTEM = """Write an engaging, great 5 scenes children's animated history. Each scene should have 1-2 sentences. 
