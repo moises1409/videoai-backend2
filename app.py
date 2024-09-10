1c53df56f3ef458875ad8937de6b3cc8a9e1fb77
moises1409 <127446413+moises1409@users.noreply.github.com>
2024-09-10 08:10:29 +0200

    test 22

--- app.py
+++ app.py
@@ -40,8 +40,8 @@ def get_test():
 def get_test2():
     return "que pacha"
 
-@app.route("/get_story", methods=['GET'])
-def generate_story():
+@app.route("/get_story2", methods=['GET'])
+def generate_story2():
     topic= "una nina llamada Isabel tiene super poderes"
     language = "Spanish"
     try:
@@ -60,6 +60,29 @@ def generate_story():
         return jsonify(response_dict)
         
         return response
+    except Exception as e:
+        print(f"Failed to generate story: {e}")
+        return None
+
+@app.route("/get_story", methods=['GET'])
+def generate_story():
+    topic= "una nina llamada Isabel tiene super poderes"
+    language = "Spanish"
+    try:
+        completion = client.beta.chat.completions.parse(
+        #model="gpt-4o-2024-08-06",
+        model = "gpt-4o-mini",
+        
+        messages=[
+            {"role": "system", "content": PROMPT_SYSTEM},
+            {"role": "user", "content": PROMPT_USER1 + topic},
+            {"role": "user", "content": PROMPT_USER2 +language}
+        ],
+        response_format=Story,
+        )
+        response = completion.choices[0].message.parsed
+        response_dict = response.model_dump()
+        return jsonify(response_dict)
     except Exception as e:
         print(f"Failed to generate story: {e}")
         return None
