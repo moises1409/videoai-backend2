964b0dcf632d0c4a633ce2c05d837b53ae214f9f
moises1409 <127446413+moises1409@users.noreply.github.com>
2024-09-21 09:26:13 +0200

    commit prompt images

--- app.py
+++ app.py
@@ -33,9 +33,14 @@ class Story(BaseModel):
     complete_story: str
 
 # Constants
-PROMPT_SYSTEM = """Write an engaging, great 5 scenes children's animated history. Each scene should have 1-2 sentences. 
-            Generate appropriate prompt to generate a coherent image for each scene. Limit of 5 scenes,
-            do not exceed 2 sentences per scene. Do not exceed 5 scenes."""
+PROMPT_SYSTEM = """Write an engaging, great 5 scenes children's animated history. Each scene should have 1-2 sentences.  
+Generate appropriate prompt to generate a coherent image for each scene. 
+The styles of all the images in the story are: Cartoon, vibrant, Pixar style. 
+Characters description should be detailed, like hair and color eyes and face. Main character in the story should be the same for all scenes. 
+Characters description must be consistent in all the scenes.
+Please add the styles in each prompt for images. Be sure that all the prompts are consistent in all the story. For example characters descriptions.
+Please respect the styles in all the scenes.
+Limit of 5 scenes, do not exceed 2 sentences per scene. Do not exceed 5 scenes."""
 PROMPT_USER1 = "Story is about"
 PROMPT_USER2 = "Create the story in the following language:"
 CHUNK_SIZE = 1024
