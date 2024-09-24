4e6d7aa1dfce4c5ab098140e7c27024170d0155c
moises1409 <127446413+moises1409@users.noreply.github.com>
2024-09-24 21:30:52 +0200

    test auto editor 14

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ import requests
 from azure.storage.blob import BlobServiceClient
 from urllib.parse import urlparse
 #from moviepy.editor import ImageClip, AudioFileClip, concatenate_videoclips
-from io import BytesIO
-from PIL import Image
+#from io import BytesIO
+#from PIL import Image
 
 app = Flask(__name__)
 CORS(app)
