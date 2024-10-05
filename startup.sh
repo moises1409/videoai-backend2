f21b018700978de9e37da80f9756c7384b870e7b
moises1409 <127446413+moises1409@users.noreply.github.com>
2024-10-05 07:14:26 +0200

    rollback 2

--- startup.sh
+++ startup.sh
@@ -6,17 +6,6 @@ bash install_ffmpeg.sh
 # Set the PATH to include FFmpeg
 export PATH="/home/site/ffmpeg:$PATH"
 
-# Install ImageMagick
-#apt-get update
-#apt-get install -y imagemagick
-
-
-# Set the PATH to include ImageMagick
-#export PATH="/usr/bin:$PATH"
-
-# Set the ImageMagick binary for MoviePy to use
-#export IMAGEMAGICK_BINARY="/usr/bin/magick"
-
 # Run the Flask application
 python app.py
 
